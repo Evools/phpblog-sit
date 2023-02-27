eaf7b0274c2d9a137d96f19db46e27ca404b6cc2
Commander <37365092+Evools@users.noreply.github.com>
2023-02-27 08:47:04 +0600

    create layout block

---
+++ pages/home.php
@@ -0,0 +1,13 @@
+<?php
+  $web_title = "Главная";
+?>
+
+
+<?php include "./layout/header.php"; ?>
+<?php include "./layout/nav.php"; ?>
+
+<div class="wrapper">
+  
+</div>
+
+<?php include "./layout/footer.php"; ?>
